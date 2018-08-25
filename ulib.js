b22b7eb04d84839b9ea9135d654202395307eba8
ura1020 <ura1020@gmail.com>
2018-08-26 04:39:27 +0900

    mylib

--- ulib.js
+++ ulib.js
@@ -1,54 +1,62 @@
 // ulib
 
+let keyEvent = function(keyCode){
+  let key = {
+    code:keyCode,
+    isDown:false,
+    isUp:true,
+    press:null,
+    release:null,
+    repeat:null,
+    removeEventAll:null,
+  }
+
+  key.downHandler = function(event) {
+    if (event.keyCode === key.code) {
+      if (key.isUp && key.press) {
+        key.press()
+      }
+      else if(key.repeat){
+        key.repeat()
+      }
+      key.isDown = true
+      key.isUp = false
+    }
+    event.preventDefault()
+  }
+
+  key.upHandler = function(event) {
+    if (event.keyCode === key.code) {
+      if (key.isDown && key.release){
+        key.release()
+      }
+      key.isDown = false
+      key.isUp = true
+    }
+    event.preventDefault()
+  }
+
+  window.addEventListener("keydown",key.downHandler,false);
+  window.addEventListener("keyup",key.upHandler,false);
+
+  key.removeEventAll = () => {
+    window.removeEventListener("keydown",key.downHandler,false);
+    window.removeEventListener("keyup",key.upHandler,false);
+  }
+  return key
+}
+
 module.exports = {
   log:function(object){
     if(!console || !console.log) return;
 
     console.log(object);
   },
-  keyboard:function(keyCode){
-    let key = {
-      code:keyCode,
-      isDown:false,
-      isUp:true,
-      press:null,
-      release:null,
-      repeat:null,
-      removeEventAll:null,
-    }
 
-    key.downHandler = function(event) {
-      if (event.keyCode === key.code) {
-        if (key.isUp && key.press) {
-          key.press()
-        }
-        else if(key.repeat){
-          key.repeat()
-        }
-        key.isDown = true
-        key.isUp = false
-      }
-      event.preventDefault()
-    }
-
-    key.upHandler = function(event) {
-      if (event.keyCode === key.code) {
-        if (key.isDown && key.release){
-          key.release()
-        }
-        key.isDown = false
-        key.isUp = true
-      }
-      event.preventDefault()
-    }
-
-    window.addEventListener("keydown",key.downHandler,false);
-    window.addEventListener("keyup",key.upHandler,false);
-
-    key.removeEventAll = () => {
-      window.removeEventListener("keydown",key.downHandler,false);
-      window.removeEventListener("keyup",key.upHandler,false);
-    }
-    return key
-  }
+  keyEvent:keyEvent,
+  leftEvent:function(){return keyEvent(37)},
+  rightEvent:function(){return keyEvent(39)},
+  upEvent:function(){return keyEvent(38)},
+  downEvent:function(){return keyEvent(40)},
+  spaceEvent:function(){return keyEvent(32)},
 }
